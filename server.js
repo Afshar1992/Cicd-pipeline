548ba903f1c276dedfcb7fa0e25d7e9f99551d0a
Naqib Afshar <naqibafshar57@gmail.com>
2026-01-29 07:07:26 +0300

    dummy1 change to all from af

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const app = express()
 
+
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "hello world changing"
+        message: "hello world from Af"
     })
 })
 
